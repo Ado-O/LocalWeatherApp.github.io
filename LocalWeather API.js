4b4498ff351420ae113338b5a44570718c033645
osijan.admir@gmail.com <osijan.admir@gmail.com>
2017-03-26 23:22:50 +0200

    add https
    upgrade https to ip api

--- LocalWeather API.js
+++ LocalWeather API.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     //poziv prvi request
-    $.getJSON("http://ip-api.com/json", function(data2) {
-        var lat = data2.lat;
-        var long = data2.lon;
+    $.getJSON("https://ipapi.co/json/", function(data2) {
+        var latitude = data2.latitude;
+        var longitude = data2.longitude;
 
         //pozivanje city i country iz prvog requesta
         var city = data2.city;
-        var country = data2.country;
+        var country = data2.region;
 
         $("#city").html(city);
         $("#country").html(country);
 
-        var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=7c277fcc664e3e7978897fdbe3830700';
+        var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=7c277fcc664e3e7978897fdbe3830700';
 
         //poziv drugi request
         $.getJSON(api, function(data) {
@@ -48,35 +48,36 @@ $(document).ready(function() {
 
             if (month <= 1 || month === 11) { //zima
                 if (weatherMain == "Snow") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/winterSnow_qlgv2g.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/winterSnow_qlgv2g.jpg")');
                 } else if (weatherMain == "Rain" || weatherMain === "Drizzle") {
-                    $('body').css('url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097337/winterRain_kv7drl.jpg")');
+                    $('body').css('url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097337/winterRain_kv7drl.jpg")');
                 } else {
-                    ('body').css('url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/winter_sakpuq.jpg")');
+                    ('body').css('url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/winter_sakpuq.jpg")');
                 }
             };
             if (month <= 4) { // proljece
                 if (weatherMain === "Clouds" || weatherMain === "Atmosphere") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097328/springCloud_mb39cu.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097328/springCloud_mb39cu.jpg")');
                 } else if (weatherMain === "Rain" || weatherMain === "Drizzle") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097333/springRain_euulus.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097333/springRain_euulus.jpg")');
                 } else {
-                  $('body').css('background-image', 'url("http://wallpapercave.com/wp/Eqhm7HZ.jpg")');                }
+                    $('body').css('background-image', 'url("https://wallpapercave.com/wp/Eqhm7HZ.jpg")');
+                }
             };
             if (month <= 9 && month >= 5) { //ljeto
                 if (weatherMain === "Clouds" || weatherMain === "Atmosphere") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097338/summerCloud_yedprb.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097338/summerCloud_yedprb.jpg")');
                 } else if (weatherMain === "Rain" || weatherMain === "Drizzle") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/summerRain_jbdu4p.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097342/summerRain_jbdu4p.jpg")');
                 } else {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097341/summer_zgrd5a.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097341/summer_zgrd5a.jpg")');
                 }
             };
             if (month === 10) { // jesen
                 if (weatherMain === "Clouds" || weatherMain === "Atmosphere") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097335/autmnCloud_py2far.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097335/autmnCloud_py2far.jpg")');
                 } else if (weatherMain === "Rain" || weatherMain === "Drizzle") {
-                    $('body').css('background-image', 'url("http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097329/autmnRain_xtgmum.jpg")');
+                    $('body').css('background-image', 'url("https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489097329/autmnRain_xtgmum.jpg")');
                 } else {
                     $('body').css('background-image', 'url("https://cloudinary.com/console/media_library#/dialog/image/upload/autmn_pgi91d")');
                 }
@@ -85,16 +86,16 @@ $(document).ready(function() {
             /*** konstrukcija za icon ***/
             switch (weatherMain) {
                 case "Thunderstorm":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Thunderstorm_uwcae4.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Thunderstorm_uwcae4.png');
                     break;
                 case "Drizzle":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Drizzle_qsgcrc.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Drizzle_qsgcrc.png');
                     break;
                 case "Rain":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Rain_kslmjg.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Rain_kslmjg.png');
                     break;
                 case "Snow":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Snow_cvef0i.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Snow_cvef0i.png');
                     break;
                 case "Clouds":
                     $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Atmosphere_fz52u7.png');
@@ -103,18 +104,18 @@ $(document).ready(function() {
                     $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Atmosphere_fz52u7.png');
                     break;
                 case "Clear":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Clear_dxpj6a.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Clear_dxpj6a.png');
                     break;
                 case "Additional":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Extreme_xdtdod.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Extreme_xdtdod.png');
                     break;
                 case "Extreme":
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Extreme_xdtdod.png');
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Extreme_xdtdod.png');
                     break;
                 default:
-                    $('#icone').attr('src', 'http://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Clear_dxpj6a.png');;
+                    $('#icone').attr('src', 'https://res.cloudinary.com/dcqcuv3gd/image/upload/v1489098018/Clear_dxpj6a.png');;
             }
-           
+
 
         });
     });
